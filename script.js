d308e05198905b1db79d4331b581ffeb846fa247
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-05-08 19:21:20 +0200

    Added check icon to both my html and javaScript

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list-container")
 
+
 document.getElementById("add-btn").addEventListener("click",addTask);
 addEventListener("keydown", (e) =>{
     if(e.key == "Enter"){
         addTask()
     }
-})
+});
+
 
 function addTask(){
     if (inputBox.value ==""){
@@ -16,12 +18,12 @@ function addTask(){
      li.innerHTML = inputBox.value;
      let span = document.createElement("span");
      span.innerHTML = "<i class='fa-solid fa-trash'></i>"
-     li.appendChild(span);
-     listContainer.appendChild(li)
+    li.appendChild(span);
+    listContainer.appendChild(li)
     }
     inputBox.value = "";
     saveData()
-    }
+} 
 
     //save data to LocalStorage
 
